59efb270b11f5b06dac7c9571ba2d4adc0704136
Sam Reha <sreha@ucsc.edu>
2016-01-16 20:47:58 -0800

    implemented addition in ubigint

--- ubigint.cpp
+++ ubigint.cpp
@@ -32,9 +32,33 @@ ubigint::ubigint (const string& that) {
    reverse(ubig_value.begin(), ubig_value.end());
 }
 
+/* Arithmetic Operations */
 ubigint ubigint::operator+ (const ubigint& that) const {
-   //return ubigint (uvalue + that.uvalue);
-   return that;
+   ubigint sum(0);
+
+   int minSize = (ubig_value.size() < that.ubig_value.size() ? ubig_value.size() : that.ubig_value.size());
+   int i = 0;        // The index in the vectors
+   int carry = 0;    // The carry value (if any) - should be reset to 0 on use
+   int digitSum = 0; // digitSum represents the sum of two digits (eg, ubig_value.at(0) + that.ubig_value.at(0))
+   while (i < minSize or carry > 0) {
+      if (i < ubig_value.size()) digitSum += ubig_value.at(i);
+      if (i < that.ubig_value.size()) digitSum += that.ubig_value.at(i);
+      digitSum += carry;
+      carry = 0;
+
+      if (digitSum > 9) { // Which is to say, if digitSum is larger than 2 digits in a base 10 counting system
+         carry = 1;
+         digitSum = digitSum % 10;
+      }
+
+      cout << carry << endl;
+      sum.ubig_value.push_back(digitSum);
+      digitSum = 0;
+      i++;
+   }
+
+   while (sum.ubig_value.size() > 0 and sum.ubig_value.back() == 0) sum.ubig_value.pop_back();
+   return sum;
 }
 
 ubigint ubigint::operator- (const ubigint& that) const {
